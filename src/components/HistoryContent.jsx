9937ab75d31dcc03d9ba56cb817805b48c603202
Hossein Ahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2025-11-14 20:55:22 +0330

    Fix: styles fixed

--- src/components/HistoryContent.jsx
+++ src/components/HistoryContent.jsx
@@ -4,7 +4,7 @@ const HistoryContent = ({ historyItems, onOpenCase }) => {
   if (!historyItems || historyItems.length === 0) {
     return null;
   }
-  console.log(historyItems);
+  // console.log(historyItems);
 
   const limitDescription = (description, maxLength = 150) => {
     if (description && description.length > maxLength) {
@@ -17,11 +17,10 @@ const HistoryContent = ({ historyItems, onOpenCase }) => {
     <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
       {historyItems.map((item, index) => (
         <div
-          key={item.id || index}
+          key={index}
           className='flex h-[243px] w-full flex-col justify-between rounded-lg bg-gray-800 p-4 shadow-lg'
         >
-          <div>
-            <h3 className='text-xl font-semibold text-gray-100'>{item.name}</h3>
+          <div className='flex-1'>
             <div className='space-y-2 text-sm text-gray-300'>
               <p className='flex justify-between'>
                 <span className='text-xl font-medium text-gray-300'>{item.shop_name}</span>
@@ -34,13 +33,15 @@ const HistoryContent = ({ historyItems, onOpenCase }) => {
                 <span className='font-medium text-gray-400'>Date:</span>
                 <span>{item.date}</span>
               </p>
-              <p className='flex flex-col gap-1'>
-                <span className='font-medium text-gray-400'>Description: </span>
-                <p>{limitDescription(item.description)}</p>
-              </p>
+              <div className='flex flex-col gap-1'>
+                <span className='font-medium text-gray-400'>Description:</span>
+                <p className='break-words whitespace-pre-wrap text-gray-300'>
+                  {limitDescription(item.description)}
+                </p>
+              </div>
             </div>
           </div>
-          <div className='mt-6 flex justify-center'>
+          <div className='mt-4 flex justify-center'>
             <button
               onClick={() => onOpenCase(item)}
               className='w-[95%] rounded-lg border border-gray-600 bg-gray-800 py-2 text-center text-sm font-medium text-blue-400 hover:bg-gray-700 hover:text-blue-300'
